2eee37b0b184460e59a727fd82ec94d7a8e8000e
Vinay V <vinayv1234@gmail.com>
2021-07-04 21:40:58 +0530

    Container with most water problem

---
+++ LeetCode/ContainerWithMostWater.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class ContainerWithMostWater
+    {
+        public int MaxArea(int[] height)
+        {
+            int area = 0, left = 0, right = height.Length - 1;
+            for(int i = 0; i < height.Length; ++i)
+            {
+                while (left < right)
+                {
+                    area = Math.Max(area, Math.Min(height[left], height[right]) * (right - left));
+                    if(height[left] < height[right])
+                    {
+                        left++;
+                    }
+                    else
+                    {
+                        right--;
+                    }
+                }
+            }
+            return area;
+        }
+    }
+}
---
+++ LeetCodeTest/ContainerWithMostWaterTests.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTest
+{
+    [TestClass]
+    public class ContainerWithMostWaterTests
+    {
+        private readonly ContainerWithMostWater container;
+        public ContainerWithMostWaterTests()
+        {
+            container = new ContainerWithMostWater();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int area = container.MaxArea(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 });
+            Assert.AreEqual(49, area);
+        }
+    }
+}
