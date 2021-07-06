a47a7a484f3fcea7119faeb85cb3cd6eccadc4b1
Vinay V <vinayv1234@gmail.com>
2021-07-06 22:46:06 +0530

    Count and say

--- LeetCode/CountSay.cs
+++ LeetCode/CountSay.cs
@@ -10,17 +10,40 @@ namespace LeetCode
     {
         public string CountAndSay(int n)
         {
-            int i = 1;
-            string s = string.Empty;
-            while(i <= n)
+            if(n > 1)
             {
-                s += RecursiveCountAndSay(i);
+                string s = CountAndSay(n - 1);
+                string formation = string.Empty;
+                List<int> nums = new List<int>();
+                int prev = 0;
+                for(int i = 0; i < s.Length; ++i)
+                {
+                    int curr = (int)char.GetNumericValue(s[i]);
+                    if (prev == curr)
+                    {
+                        int count = nums[nums.Count - 2];
+                        nums.RemoveAt(nums.Count - 1);
+                        nums.RemoveAt(nums.Count - 1);
+                        nums.Add(count+1);
+                        nums.Add(curr);
+                    }
+                    else
+                    {
+                        nums.Add(1);
+                        nums.Add(curr);
+                    }
+                    prev = curr;
+                }
+                for(int i = 0; i < nums.Count; ++i)
+                {
+                    formation += Convert.ToString(nums[i]);
+                }
+                return formation;
             }
-            return "";
-        }
-        public string RecursiveCountAndSay(int n)
-        {
-
+            else
+            {
+                return "1";
+            }
         }
     }
 }
--- LeetCodeTest/CountSayTests.cs
+++ LeetCodeTest/CountSayTests.cs
@@ -24,5 +24,77 @@ namespace LeetCodeTest
             string s = countSay.CountAndSay(n);
             Assert.AreEqual("1", s);
         }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int n = 2;
+            string s = countSay.CountAndSay(n);
+            Assert.AreEqual("11", s);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int n = 3;
+            string s = countSay.CountAndSay(n);
+            Assert.AreEqual("21", s);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            int n = 4;
+            string s = countSay.CountAndSay(n);
+            Assert.AreEqual("1211", s);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            int n = 5;
+            string s = countSay.CountAndSay(n);
+            Assert.AreEqual("111221", s);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            int n = 6;
+            string s = countSay.CountAndSay(n);
+            Assert.AreEqual("312211", s);
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            int n = 7;
+            string s = countSay.CountAndSay(n);
+            Assert.AreEqual("13112221", s);
+        }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+            int n =8;
+            string s = countSay.CountAndSay(n);
+            Assert.AreEqual("1113213211", s);
+        }
+
+        [TestMethod]
+        public void TestMethod9()
+        {
+            int n = 9;
+            string s = countSay.CountAndSay(n);
+            Assert.AreEqual("31131211131221", s);
+        }
+
+        [TestMethod]
+        public void TestMethod10()
+        {
+            int n = 10;
+            string s = countSay.CountAndSay(n);
+            Assert.AreEqual("13211311123113112211", s);
+        }
     }
 }
